ea018ed4ad682aadfadb954809da57b870e43711
Takahashi Katsunori <katsutennis.916@gmail.com>
2021-09-03 18:45:56 +0900

    明細文字制限緩和

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -8,7 +8,7 @@ class Detail < ApplicationRecord
   validates :replayer, presence: true
   validates :status, presence: true
   validates :classification, presence: true
-  validates :note, length: { maximum: 10 }
+  validates :note, length: { maximum: 50 }
   enum classification: { rent: 0, food: 1, life: 2, enjoy: 3, money: 4, other: 5 }
   enum status: { not_yet: 0, done: 1 }
 end
