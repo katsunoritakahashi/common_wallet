640fe9512d3dd3050443c6de31df095c093a6210
Takahashi Katsunori <katsutennis.916@gmail.com>
2021-10-24 14:28:24 +0900

    [fix]新規作成と同時にログイン出来るようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       redirect_back_or_to months_path, success: '登録が完了しました'
     else
       flash.now[:danger] = '登録ができませんでした'
