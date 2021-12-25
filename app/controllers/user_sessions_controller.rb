6e77d698401d932239f68ab31869de1b7337f4d7
Takahashi Katsunori <katsutennis.916@gmail.com>
2021-12-25 23:44:46 +0900

    ログインしました文言を削除

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to profile_path, success: 'ログインしました'
+      redirect_back_or_to profile_path
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
