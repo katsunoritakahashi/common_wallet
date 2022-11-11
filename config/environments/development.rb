d92d65d3fbc94f28c16f1bac96261ff2f25d5e9b
Takahashi Katsunori <katsutennis.916@gmail.com>
2022-11-12 00:16:22 +0900

    [fix]DBの環境変数を設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  config.hosts << "containers-us-west-102.railway.app"
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
